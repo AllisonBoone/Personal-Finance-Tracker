42bc8de0a07ea95e5d26a5803b337512d88747e4
Allison <Allison.Boone@keyin.com>
2024-10-16 17:18:51 -0230

    installed bootstrap and made some style changes to App.js

--- personal-budget-tracker/src/App.js
+++ personal-budget-tracker/src/App.js
@@ -2,19 +2,29 @@ import React from 'react';
 import Header from './components/Header.jsx';
 import Summary from './components/Summary.jsx';
 import TransactionForm from './components/TransactionForm.jsx';
-import TransactionList from './components/TransactionList.jsx';
 import Chart from './components/Chart.jsx';
+import TransactionList from './components/TransactionList.jsx';
 import { BudgetProvider } from './components/BudgetContext';
 
 function App() {
   return (
     <BudgetProvider>
-      <div className="App">
+      <div className="container">
         <Header />
-        <Summary />
-        <Chart />
-        <TransactionForm />
-        <TransactionList />
+        <div className="row mt-4">
+          <div className="col-md-6">
+            <Summary />
+            <TransactionForm />
+          </div>
+          <div className="col-md-6">
+            <Chart />
+          </div>
+        </div>
+        <div className="row mt-4">
+          <div className="col">
+            <TransactionList />
+          </div>
+        </div>
       </div>
     </BudgetProvider>
   );
